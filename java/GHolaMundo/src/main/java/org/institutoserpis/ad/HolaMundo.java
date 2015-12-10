4771eb12ba8de658042cd3435516b2d940ef7a20
macortes <miguelcortgil@gmail.com>
2015-12-10 10:28:13 +0100

    Añadido HolaMundo

---
+++ java/GHolaMundo/src/main/java/org/institutoserpis/ad/HolaMundo.java
@@ -0,0 +1,11 @@
+package org.institutoserpis.ad;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hola mundo desde eclipse con Gradle");
+
+	}
+
+}
